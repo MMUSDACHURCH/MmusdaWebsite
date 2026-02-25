70e03965e556c56123a80ff2f537eb45814daf8a
EmmanuelMose <emmanuelmose806@gmail.com>
2026-02-25 11:50:31 +0300

    165 commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import Events from "./components/events/Events";
 import AboutSDA from "./components/aboutsda/AboutSDA";
 import Books from "./components/book/Book";
 import Departments from "./components/departments/Departments";
+import Leaders from "./components/leaders/Leaders";
 
 function App() {
   const router = createBrowserRouter([
@@ -65,6 +66,11 @@ function App() {
       path: "/departments",
       element: <Departments />,
     },
+    {
+      path: "/leadership",
+      element: <Leaders />,
+    },
+    
   ]);
 
   return (
