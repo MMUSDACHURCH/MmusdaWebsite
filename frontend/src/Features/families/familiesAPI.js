98a8676fc4c4b53adf2a040408383f6c7ac80a20
EmmanuelMose <emmanuelmose806@gmail.com>
2026-03-10 16:48:08 +0300

    380 commit

--- frontend/src/Features/families/familiesAPI.js
+++ frontend/src/Features/families/familiesAPI.js
@@ -1,12 +1,9 @@
-// src/Features/families/familiesAPI.js
 import { APIDomain } from "../../utils/APIDomain";
 
 export const fetchFamilies = async () => {
   try {
     const response = await fetch(`${APIDomain}/api/families`);
-    if (!response.ok) {
-      throw new Error("Failed to fetch families");
-    }
+    if (!response.ok) throw new Error("Failed to fetch families");
     const data = await response.json();
     return data;
   } catch (error) {
