0da09cad3b3a15dad3c6c1ce5d686d52fbebc630
EmmanuelMose <emmanuelmose806@gmail.com>
2026-03-12 09:48:33 +0300

    410 commit

--- backend/src/mailer.js
+++ backend/src/mailer.js
@@ -3,10 +3,7 @@ import nodemailer from "nodemailer";
 export const sendVerificationEmail = async (email, code) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      family: 4, 
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
@@ -27,3 +24,4 @@ export const sendVerificationEmail = async (email, code) => {
     throw new Error("Failed to send email");
   }
 };
+
